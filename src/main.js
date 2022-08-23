4254c316162cee65e5a9abb6693bcd0e2e04ddbf
morales <franciscomoralessanchez00@gmail.com>
2022-08-22 21:30:18 -0600

    Lista de peliculas recomendadas

--- src/main.js
+++ src/main.js
@@ -126,4 +126,13 @@ async function getMovieById(id) {
     movieDetailScore.textContent = movie.vote_average;
 
     createCategories(movie.genres, movieDetailCategoriesList);
+
+    getRelateMoviesId(id);
+}
+
+async function getRelateMoviesId(id) {
+    const { data } = await api(`movie/${id}/recommendations`);
+    const relatedMovies = data.results;
+
+    createMovies(relatedMovies, relatedMoviesContainer);
 }
