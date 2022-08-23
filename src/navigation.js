122f62bf30dbd5e72be34cfea73803d257d5be4c
morales <franciscomoralessanchez00@gmail.com>
2022-08-22 21:06:45 -0600

    Endpoint de detalles de una pelicula

--- src/navigation.js
+++ src/navigation.js
@@ -91,6 +91,10 @@ function movieDetailsPage() {
     categoriesPreviewSection.classList.add('inactive');
     genericSection.classList.add('inactive');
     movieDetailSection.classList.remove('inactive');
+
+     // ['#category', '436587']
+     const [_, movieId] = location.hash.split('=');
+     getMovieById(movieId);
 }
 
 
